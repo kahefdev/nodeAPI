9e9639de276142f54fa8dee5289d64def50c38e5
phoenixveracity <phoenix_veracity@yahoo.com>
2020-05-31 21:18:45 +1000

    re-organized files

--- app.js
+++ app.js
@@ -4,31 +4,25 @@ const app = express();
 const morgan = require('morgan');
 const toursRouter = require('./routes/toursRouter');
 const usersRouter = require('./routes/usersRouter');
-const lptRouter = require('./routes/lptRouter.js');
-const industryRouter = require('./routes/industryRouter.js');
 
-const pgRouter = require('./routes/pgRouter.js');
-
-const { pracNode } = require('./controllers/pracNode.js');
-const { getOverview } = require('./controllers/lptController');
-const { getUsers } = require('./queries.js');
 // app.use(morgan('dev'));
 app.use(express.static(`${__dirname}/public/`));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.getTime = new Date().toISOString();
-  console.log(req.getTime);
-  next();
-});
-
-app.get('/node', pracNode);
-app.use('/lpt', lptRouter);
-app.use('/industry', industryRouter);
-app.use('/dvdrental/users', pgRouter);
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
-app.get('/users/:id', getUsers);
-app.get('/overview', getOverview);
 
 module.exports = app;
+
+// const { getUsers } = require('./queries.js');
+// const lptRouter = require('./routes/lptRouter.js');
+// const { pracNode } = require('./controllers/pracNode.js');
+// const pgRouter = require('./routes/pgRouter.js');
+// const industryRouter = require('./routes/industryRouter.js');
+// const { getOverview } = require('./controllers/lptController');
+// app.get('/overview', getOverview);
+// app.get('/node', pracNode);
+// app.use('/lpt', lptRouter);
+// app.use('/industry', industryRouter);
+// app.use('/dvdrental/users', pgRouter);
+// app.get('/users/:id', getUsers);
