901b05b30f956d4323c9fe5d88f9fcc6ad697894
phoenixveracity <phoenix_veracity@yahoo.com>
2020-06-12 21:15:19 +1000

    Test

--- utils/AppError.js
+++ utils/AppError.js
@@ -4,6 +4,7 @@ class AppError extends Error {
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
+    console.log('Testing stuff');
     Error.captureStackTrace(this, this.constructor);
   }
 }
