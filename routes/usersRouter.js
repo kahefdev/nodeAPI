c42852f1260063acf4f79b0bc5037ea571e3881b
phoenixveracity <phoenix_veracity@yahoo.com>
2020-06-14 02:58:11 +1000

    refactoring

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -21,7 +21,7 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.post('/forgotPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
-router.patch('/updatePassword', updatePassword);
+router.patch('/updatePassword', protect, updatePassword);
 
 router
   .route('/')
