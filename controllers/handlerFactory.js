8f9a2df69b63960689745e6a869fbed728c62a04
Phoenix <phoenix@Phoenixs-MacBook-Air.local>
2020-07-10 01:08:39 +1000

    deleteFactoryFunction

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,15 +1,13 @@
 const catchAsync = require("../utils/catchAsync");
-
+const AppError = require("../utils/AppError");
 
 exports.deleteOne = Model => catchAsync(async (req, res, next) => {
     let { id } = req.params;
-    console.log(id);
-    let tour = await Tour.findByIdAndDelete(id);
-  
-    if (!tour) return next(new AppError('Tour not found', 404));
+    let doc = await Model.findByIdAndDelete(id);
+    if (!doc) return next(new AppError('Tour not found', 404));
   
     res
       .status(200)
-      .json({ status: 'Success', message: 'User deleted successfully' });
+      .json({ status: 'Success', message: 'Item deleted successfully' });
   });
 
