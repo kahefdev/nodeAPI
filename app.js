d4cc615874de7fc102a4c0823709222811d4d035
Kahef <kahef@MacBook-Pro.local>
2020-07-18 17:07:23 +1000

    Installing templage engine - PUG

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
-
+const path = require('path');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -13,6 +13,13 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+// 0)To set the template engine
+app.set('view engine','pug');
+
+app.set('views', path.join(__dirname,'views'));
+
+console.log(`${__dirname}`);
 
 // 1) GLOBAL MIDDLEWARES
 // Set security HTTP headers
@@ -55,7 +62,7 @@ app.use(
 );
 
 // Serving static files
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname,'public')));
 
 // Test middleware
 app.use((req, res, next) => {
@@ -68,6 +75,10 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+
+app.get('/',(req,res)=>{
+  res.status(200).render('base');
+})
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
--- server.js
+++ server.js
@@ -10,12 +10,12 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+    const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+    );
 
-mongoose
+  mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
@@ -25,6 +25,7 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
+
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
